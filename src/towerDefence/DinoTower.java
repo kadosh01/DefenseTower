d06251c49e558b2755e6e2cd49318e04a05dc833
khgjhgh <kadosh01@gmail.com>
2017-06-25 23:27:58 +0300

    23:27

--- src/towerDefence/DinoTower.java
+++ src/towerDefence/DinoTower.java
@@ -2,9 +2,9 @@ package towerDefence;import java.awt.Image;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-public class MagicTower extends Tower{
-	private final String[] path={"/Tower/Magic/Layer 1.png","/Tower/Magic/Layer 2.png","/Tower/Magic/Layer 3.png","/Tower/Magic/Layer 4.png","/Tower/Magic/Layer 5.png","/Tower/Magic/Layer 6.png","/Tower/Magic/Layer 7.png","/Tower/Magic/Layer 8.png","/Tower/Magic/Layer 9.png","/Tower/Magic/Layer 10.png","/Tower/Magic/Layer 11.png"};
-	public MagicTower(int x, int y) throws IOException {
+public class DinoTower extends Tower{
+	private final String[] path={"/Tower/Dino/Layer 1.png","/Tower/Dino/Layer 2.png","/Tower/Dino/Layer 3.png","/Tower/Dino/Layer 4.png","/Tower/Dino/Layer 5.png","/Tower/Dino/Layer 6.png","/Tower/Dino/Layer 7.png"};
+	public DinoTower(int x, int y) throws IOException {
 		super(x, y);
 		this.hitArea=1;
 		m=new Image[path.length];
